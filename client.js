72689d59adea2098eb697b1149a6078e3cb667b8
dsawyer13 <dsawyer@tutanota.com>
2019-01-17 19:11:08 -0500

    updated source of javascript in index.html

--- client.js
+++ client.js
@@ -1,17 +1,18 @@
-// 'use strict';
-//
-// function createAccount() {
-//   $('signup-submit').click(function() {
-//     let username = $('.uname').val();
-//     let password = $('.pwd').val();
-//     let firstName = $('.firstName').val();
-//     let lastName = $('.lastName').val();
-//     let regData = {'username': username, 'password': password, 'firstName': firstName, 'lastName': lastName};
-//     $.ajax({
-//       method: 'POST',
-//       url: '/api/users',
-//       data: regData,
-//       success: function(data){
-//     }
-//   })
-// }
+'use strict';
+
+function createAccount() {
+  $('signup-submit').click(function() {
+    let username = $('.uname').val();
+    let password = $('.pwd').val();
+    let firstName = $('.firstName').val();
+    let lastName = $('.lastName').val();
+    let regData = {'username': username, 'password': password, 'firstName': firstName, 'lastName': lastName};
+    $.ajax({
+      method: 'POST',
+      url: '/api/users',
+      data: regData,
+      success: function(response){
+        alert(response)
+    }
+  })
+}
