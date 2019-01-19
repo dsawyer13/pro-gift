da153d825bd4f6c0f824019001c3f596ec4ef591
dsawyer13 <dsawyer@tutanota.com>
2019-01-18 21:11:04 -0500

    changed database_url

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/giftRegistry-dev';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/giftrRegistry-dev';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost:27017/giftRegistry-test'
 exports.PORT = process.env.PORT || 3000;
 exports.JWT_SECRET = process.env.JWT_SECRET || 'hiitme';
