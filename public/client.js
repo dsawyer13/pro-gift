e2b3b4e4a78efdaa52ca782ab09aa4c7e837c34e
dsawyer13 <dsawyer@tutanota.com>
2019-01-19 14:37:00 -0500

    testing that heroku works

--- public/client.js
+++ public/client.js
@@ -1,18 +1,27 @@
 'use strict';
 
-function createAccount() {
-  $('signup-submit').click(function() {
-    let username = $('.uname').val();
-    let password = $('.pwd').val();
-    let firstName = $('.firstName').val();
-    let lastName = $('.lastName').val();
-    let regData = {'username': username, 'password': password, 'firstName': firstName, 'lastName': lastName};
-    $.ajax({
-      method: 'POST',
-      url: '/api/users',
-      data: regData,
-      success: function(response){
-        alert(response)
-    }
+// function createAccount() {
+//   $('signup-submit').click(function() {
+//     let username = $('.uname').val();
+//     let password = $('.pwd').val();
+//     let firstName = $('.firstName').val();
+//     let lastName = $('.lastName').val();
+//     let regData = {'username': username, 'password': password, 'firstName': firstName, 'lastName': lastName};
+//     $.ajax({
+//       method: 'POST',
+//       url: '/api/users',
+//       data: regData,
+//       success: function(response){
+//         alert(response)
+//     }
+//   })
+// }
+
+function testHeroku() {
+  $('signup-submit').on('submit', function() {
+    event.preventDefault();
+    alert('Hello World');
   })
-}
+};
+
+$(testHeroku);
