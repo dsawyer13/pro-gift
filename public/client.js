55fa28d535f9373e8330b12e9c2fef87e1434009
dsawyer13 <dsawyer@tutanota.com>
2019-01-19 15:43:37 -0500

    reverted changes

--- public/client.js
+++ public/client.js
@@ -1,23 +1,35 @@
+
 'use strict';
 
-function createAccount() {
-  $('.signup-form').on('submit', 'signup-button', function(event) {
+$(document).ready(function() {
+
+  $('.signup-form').submit(function(event) {
     event.preventDefault();
+    ajaxPost();
+  });
 
-    // let username = $('.uname').val();
-    // let password = $('.pwd').val();
-    // let firstName = $('.firstName').val();
-    // let lastName = $('.lastName').val();
-    // let regData = {'username': username, 'password': password, 'firstName': firstName, 'lastName': lastName};
-    $.ajax({
-      type: 'POST',
-      url: '/api/users',
-      data: $('.signup-form').serialize(),
+  function ajaxPost() {
 
-    }).done(response => {
-      console.log(response)
-    })
-  })
-}
+      let username = $('.uname').val();
+      let password = $('.pwd').val();
+      let firstName = $('.firstName').val();
+      let lastName = $('.lastName').val();
+      let formData = {'username': username, 'password': password, 'firstName': firstName, 'lastName': lastName};
 
-$(createAccount);
+      $.ajax({
+        type: 'POST',
+        url: window.location + '/api/users',
+        data: JSON.stringify(formData),
+        dataType: 'json',
+        success: function(data){
+          $('.test').html("<p>" +
+        data.username + "</p>");
+      },
+      error: function(e) {
+        alert("Error")
+        console.log("Error: ", e);
+      }
+    });
+  }
+
+})
