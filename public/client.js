6c4136c934b524e39fdf4a92ae4b89113c398cd0
dsawyer13 <dsawyer@tutanota.com>
2019-01-19 15:47:09 -0500

    changed url

--- public/client.js
+++ public/client.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
 
       $.ajax({
         type: 'POST',
-        url: window.location + '/api/users',
+        url: 'https://powerful-mountain-84317.herokuapp.com/api/users',
         data: JSON.stringify(formData),
         dataType: 'json',
         success: function(data){
