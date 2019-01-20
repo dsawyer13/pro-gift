29aab78873bbd29c7df8b125b52ed260d08b4f73
dsawyer13 <dsawyer@tutanota.com>
2019-01-19 19:13:24 -0500

    still fixing this junk

--- public/client.js
+++ public/client.js
@@ -3,41 +3,35 @@
 
 
 
-  function handleSignupSubmit() {
-
+  function createAccount() {
     $('.signup-form').submit(function(e) {
       e.preventDefault();
 
-      createAccount({
+      console.log(userInfo);
+
+      const formData = {
         username: $(e.currentTarget).find('.username').val(),
         password: $(e.currentTarget).find('.password').val(),
         firstName: $(e.currentTarget).find('.firstName').val(),
         lastName: $(e.currentTarget).find('.lastName').val()
-      });
-    });
-  }
+      };
 
-  function createAccount(userInfo) {
-
-    console.log(userInfo);
-
-    $.ajax({
-      method: 'POST',
-      url: '/api/users',
-      data: JSON.stringify(data),
-      success: function(data) {
-        console.log(data)
-      },
-      dataType: 'json',
-      contentType: 'application/json'
-    })
-  };
+      $.ajax({
+        method: 'POST',
+        url: '/api/users',
+        data: JSON.stringify(formData),
+        success: function(data) {
+          console.log(data)
+        },
+        dataType: 'json',
+        contentType: 'application/json'
+      })
+  })};
 
 
 
 
 $(function() {
-  handleSignupSubmit();
   createAccount();
 
 })
