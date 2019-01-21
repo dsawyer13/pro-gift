b3c2f2dc4740cbf4848364ab3685d70df6e0c474
dsawyer13 <dsawyer@tutanota.com>
2019-01-21 18:41:25 -0500

    added another console.log for testing

--- public/client.js
+++ public/client.js
@@ -67,7 +67,8 @@ function createAccount() {
 function authenticateUser(data) {
   //store token in localStorage and put it in header for GET request
   console.log(data);
-  const token = data[0];
+  const token = data[0].authToken;
+  console.log(token);
   localStorage.setItem('token', token);
 
   $.ajax({
